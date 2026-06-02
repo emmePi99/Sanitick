efeb1d6339cf115d0b3728c4e84cd23af2d42e24
emmePi99 <riccardo.bernardi@buonarroti.tn.it>
2026-06-02 15:53:03 +0200

    #8: Resolved https://github.com/emmePi99/Sanitick/pull/33#discussion_r3341590508

--- backend/src/modules/api/auth/dto/auth/change-password.dto.ts
+++ backend/src/modules/api/auth/dto/auth/change-password.dto.ts
@@ -14,7 +14,7 @@ export class ChangePasswordDto {
   })
   @IsString({ message: 'La password deve essere una stringa valida' })
   @MinLength(8, { message: 'La password deve contenere almeno 8 caratteri' })
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+  @Matches(/(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message:
       'La password è troppo debole. Deve contenere almeno una maiuscola, una minuscola, un numero e un carattere speciale',
   })
